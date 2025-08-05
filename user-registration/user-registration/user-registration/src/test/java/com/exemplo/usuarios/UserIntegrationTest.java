146f964361241547d77bff8054d4089e42db5a16
DiogoCamps <diogoapferreiracampos@gmail.com>
2025-08-05 17:30:42 -0300

    att teste

--- user-registration/user-registration/user-registration/src/test/java/com/exemplo/usuarios/UserIntegrationTest.java
+++ user-registration/user-registration/user-registration/src/test/java/com/exemplo/usuarios/UserIntegrationTest.java
@@ -9,6 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserIntegrationTest {
 
+    @Test
+    void testFullUserFlow() {
+        UserRepository repository = new UserRepository();
+        UserService service = new UserService(repository);
 
-
+        UserController userController = new UserController(service);
+        userController.createUser("2","Diogo");
+        User user = userController.findUser("2");
+        assertNotNull(user);
+        userController
+    }
 }
